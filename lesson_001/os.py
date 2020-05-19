d8b17fceaee3136509900544b92723ba05fb1734
noteppp <exezz@yandex.ru>
2020-05-19 23:05:58 +0500

    Полностью версия системы ggg

--- lesson_001/os.py
+++ lesson_001/os.py
@@ -1,3 +1,11 @@
-#this file about python
+import platform
 import sys
-print(sys.version_info)
+
+info = 'OS info is \n{}\n\nPython version is {} {}'.format(
+    platform.uname(), sys.version, platform.architecture())
+print(info)
+
+with open('os.txt', 'w') as ff:
+    ff.write(info)
+o = 1222122121 + 444445454
+print(o)
